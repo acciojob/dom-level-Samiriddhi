33641fd292c72347485cb1eee5a5d1855b58a08a
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-07-25 01:14:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //your JS code here. If required.
 
 
-let s=document.getElementById("level").innerText;
-let ans=s[s.length-1];
-alert("The level of the element is: "+ans);
+// let s=document.getElementById("level").innerText;
+// let ans=s[s.length-1];
+// alert("The level of the element is: "+ans);
+
+
+if(document.getElementsByTagName("ul")=="Item 5"){
+	alert("The level of the element is: "+ ul[ul.length-1]);
+}
